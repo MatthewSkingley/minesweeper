58eb01218afd16a8ba3d30b8eec83f3fcb150443
Matthew Skingley <skingleymatthew@gmail.com>
2019-07-23 19:24:25 +0100

    First playable version! Fixed flag behavior, Re-factored getting of surrounding elements, Added win condition

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // Javascript Based Minesweeper
 
 function setup(width, height) {
+	Window.bombCount = 0;
 	Window.a = createArray(width, height);
 	Window.width = width;
 	Window.height = height;
 	printArray();
+	Window.blankCount = width*height - Window.bombCount;
 }
 
 function dispBox(feature) {
@@ -23,21 +25,30 @@ class Box extends HTMLElement {
 		super();
 		this.coordinates = null;
 		this.addEventListener("click", function(event) {
-			console.log("abs-pos = " + this.z 
-			+ " x-pos = " + this.x
-			+ " y-pos = " + this.y);
-			if(Window.a[this.y][this.x] == "b") {
-				alert("bombed");
-				location.reload();
-			} else {
-				this.zeroTest();
+		    if (this.innerHTML == "") {
+				// console.log("abs-pos = " + this.z 
+				// + " x-pos = " + this.x
+				// + " y-pos = " + this.y);
+				if(Window.a[this.y][this.x] == "b") {
+					alert("bombed");
+					location.reload();
+				} else {
+					this.zeroTest();
+				}
+				if(Window.count == Window.blankCount) {
+					alert("You won!");
+				}
+			}
+			var hit = Window.zArray.filter( el => !isNaN(parseInt(el.innerHTML))).length;
+			if(hit == Window.blankCount) {
+				alert("chicken dindins");
 			}
 		});
 		this.addEventListener('contextmenu', function(event) { 
 			event.preventDefault();
-			if (this.innerHTML != "\u2690") {
+			if (!this.innerHTML) {
 				this.innerHTML = "\u2690";
-			} else {
+			} else if(this.innerHTML == "\u2690") {
 				this.innerHTML = null;
 			}
 			return false;
@@ -59,72 +70,16 @@ class Box extends HTMLElement {
 	}
 
 	zeroTest() {
-		var bombs = get(this).filter(cell => cell == "b").length;
+		var bombs = get(this).filter(el => Window.a[el.y][el.x] == "b").length;
 		if(bombs != 0) {
 			this.innerHTML = bombs;
-		} else if(Window.a[this.y][this.x] != '0') {
+		} else if(Window.a[this.y][this.x] != '0' && !parseInt(this.innerHTML)) {
 			Window.a[this.y][this.x] = '0';
 			this.innerHTML = 0;
-			var zArray = Window.zArray;
-			var w = Window.width;
-			var h = Window.height;
-			var x = this.x;
-			var y = this.y;
-			var z = this.z;
-
-			Window.s = [];
-			function getNorth() {
-				if(y != 0) {
-					return zArray[z-w];
-				}
-			}
-			Window.s.push(getNorth());
-			function getNorthEast() {
-				if(x != w-1 && y != 0) {
-					return zArray[z-w+1];
-				}
-			} 
-			Window.s.push(getNorthEast());
-			function getEast() {
-				if(x != w-1) {
-					return zArray[z+1];
-				}
-			}
-			Window.s.push(getEast());
-			function getSouthEast() {
-				if(x != w-1 && y != h-1) {
-					return zArray[z+w+1];
-				}
-			}
-			Window.s.push(getSouthEast());
-			function getSouth() {
-				if(y != h-1) {
-					return zArray[z+w];
-				}
-			}
-			Window.s.push(getSouth());
-			function getSouthWest() {
-				if(x != 0 && y != h-1) {
-					return zArray[z+w-1];
-				}
-			}
-			Window.s.push(getSouthWest());
-			function getWest() {
-				if(x != 0) {
-					return zArray[z-1];
-				}
-			}
-			Window.s.push(getWest());
-			function getNorthWest() {
-				if(x != 0 && y != 0) {
-					return zArray[z-w-1];
-				}
-			}
-			Window.s.push(getNorthWest());
-			Window.s.forEach(function(element) {
+			var ess = get(this);
+			ess.forEach(function(element) {
 				console.log(element);
 				if(element) {
-					console.log("true");
 					element.zeroTest();
 				}
 			});
@@ -139,10 +94,11 @@ function createArray(width, height) {
 	function createX(width){
 		var x = [];
 		for(var i = 0; i < width; i++) {
-			if(Math.random() <= 0.9) {
+			if(Math.random() <= 0.85) {
 				x.push(" ");
 			} else {
 				x.push("b");
+				Window.bombCount++;
 			}
 		}
 	return x;	
@@ -169,18 +125,62 @@ function printArray() {
 	});
 }
 
-function get(cell) {
-	var y = cell.y;
-	var x = cell.x;
-	var a = Window.a;
-	var s = [];
-	try{s.push(a[y-1][x-1]	); } catch(ignore){}
-	try{s.push(a[y-1][x]		); } catch(ignore){}
-	try{s.push(a[y-1][x+1]	); } catch(ignore){}
-	try{s.push(a[y]	[x-1]	); } catch(ignore){}
-	try{s.push(a[y]	[x+1]	); } catch(ignore){}
-	try{s.push(a[y+1][x-1]	); } catch(ignore){}
-	try{s.push(a[y+1][x]		); } catch(ignore){}
-	try{s.push(a[y+1][x+1]	); } catch(ignore){}
-	return s;
+function get(Box) {
+	var zArray = Window.zArray;
+	var w = Window.width;
+	var h = Window.height;
+	var x = Box.x;
+	var y = Box.y;
+	var z = Box.z;
+
+	Window.s = [];
+	function getNorth() {
+		if(y != 0) {
+			Window.s.push(zArray[z-w]);
+		}
+	}
+	getNorth();
+	function getNorthEast() {
+		if(x != w-1 && y != 0) {
+			Window.s.push(zArray[z-w+1]);
+		}
+	}
+	getNorthEast(); 
+	function getEast() {
+		if(x != w-1) {
+			Window.s.push(zArray[z+1]);
+		}
+	}
+	getEast();
+	function getSouthEast() {
+		if(x != w-1 && y != h-1) {
+			Window.s.push(zArray[z+w+1]);
+		}
+	}
+	getSouthEast();
+	function getSouth() {
+		if(y != h-1) {
+			Window.s.push(zArray[z+w]);
+		}
+	}
+	getSouth();
+	function getSouthWest() {
+		if(x != 0 && y != h-1) {
+			Window.s.push(zArray[z+w-1]);
+		}
+	}
+	getSouthWest();
+	function getWest() {
+		if(x != 0) {
+			Window.s.push(zArray[z-1]);
+		}
+	}
+	getWest();
+	function getNorthWest() {
+		if(x != 0 && y != 0) {
+			Window.s.push(zArray[z-w-1]);
+		}
+	}
+	getNorthWest();
+	return Window.s;
 }
